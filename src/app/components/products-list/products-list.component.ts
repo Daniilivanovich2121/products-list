48db92a2f7418c0e0379c104137dd534d789e608
пвпвавпвапва <pritulodanil@gmail.com>
2025-05-10 18:14:26 +0300

    логика ngrx+state

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,8 +1,30 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {BasketService} from '../../services/basket.service';
+import {Store} from '@ngrx/store';
+import {selectProducts, selectProductsError, selectProductsStatus} from '../../store/products.selector';
+import {createProduct, deleteProduct, editProduct, getProducts} from '../../store/products.action';
+import {CreateProductModels, Product} from '../../models/productModel';
+import {CreateProductsDialogComponent} from '../create-products-dialog/create-products-dialog.component';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton, MatFabButton} from '@angular/material/button';
+import {PushPipe} from '@ngrx/component';
+import {MatProgressBar} from '@angular/material/progress-bar';
+import {ProductsCardComponent} from '../products-card/products-card.component';
+import {BasketProductsComponent} from '../basket-products/basket-products.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-products-list',
   imports: [
-
+    MatIcon,
+    MatButton,
+    MatFabButton,
+    PushPipe,
+    MatProgressBar,
+    ProductsCardComponent,
+    BasketProductsComponent,
+    MatSidenavModule
   ],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
