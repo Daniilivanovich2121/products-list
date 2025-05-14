8dd73746f758f8220720d4ee1d1784a401b735d2
пвпвавпвапва <pritulodanil@gmail.com>
2025-05-14 13:26:57 +0300

    логика ngrx+state

---
+++ src/app/services/api-user.service.ts
@@ -0,0 +1,30 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CreateProductModels, Product} from '../models/productModel';
+import {API_URL} from '../models/api-url';
+import {User} from '../models/userModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private readonly http = inject(HttpClient)
+
+  getUser() {
+    return this.http.get<User>(API_URL + 'user')
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete<boolean>(API_URL + 'user/' + id)
+  }
+
+  createUser(product: CreateProductModels) {
+    return this.http.post<User>(API_URL + 'user/', product)
+  }
+
+  updateUser(product: Product) {
+    console.log(product)
+    return this.http.put<Product>(API_URL + `user/${product.id}`, product)
+  }
+}
